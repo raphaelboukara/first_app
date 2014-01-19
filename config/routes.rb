d39c6d53a4260383b0cb182d887fc4931b6247d0
Raphael Boukara <raphael.boukara@gmail.com>
2014-01-19 12:47:13 +0200

    une petite demo qui va marcher heingit add .

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 FirstApp::Application.routes.draw do
   resources :users
-
-  match '/', to: 'users#index', via: 'get'
+  
+  root  'users#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
